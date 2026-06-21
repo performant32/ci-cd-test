2dc7cbcffdf346609612bd9811c12cbdd43596fd
performant32 <performant32@gmail.com>
2026-06-21 17:58:23 -0500

    Server workign ok

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello world");
+const { error } = require("console");
+const express = require("express");
+const fs = require('fs')
+
+const port = process.env.PORT | 8080
+const server = express();
+
+server.listen(port, (err, reason)=>{
+    if(err)
+        console.error(`Failed to listen on port ${port}, err ${reason}`)
+    console.log("Started server on port " + port)
+})
+server.get("/", (req, res)=>{
+    try{
+        res.writeHead(200, {"Content-Type":"text/html"})
+        res.write(`Hello client ${req.url}`)
+        res.end();
+    }
+    catch(exception){
+        console.log("Error handling client")
+    }
+})
